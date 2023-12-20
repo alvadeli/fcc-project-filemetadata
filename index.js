5d8e15774348b7bc09da58b8114bcbb0fa8fa820
alvadeli <142695804+alvadeli@users.noreply.github.com>
2023-12-20 23:20:44 +0100

    Finished Taks

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+var multer = require("multer");
+var bodyParser = require("body-parser");
+require('dotenv').config();
 
 var app = express();
+const storage = multer.memoryStorage(); 
+const upload = multer({ storage: storage });
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
@@ -12,9 +17,18 @@ app.get('/', function (req, res) {
 });
 
 
+app.post("/api/fileanalyse", upload.single('upfile'),(req,res) =>{
+  const file = req.file;
+  console.log('File received:', file);
+
+  res.json({name: file.originalname, type: file.mimetype, size: file.size});
+})
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+var listener = app.listen(port, function () {
+  console.log('Your app is listening on port ' + port);
+  //console.log(`Server is running at http://localhost:${listener.address().port}`);
 });
+
+
